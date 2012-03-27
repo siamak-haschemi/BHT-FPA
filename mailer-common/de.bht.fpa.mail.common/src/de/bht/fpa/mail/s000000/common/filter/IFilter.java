3099bcde8870cba87956c23aa09c96656034ad0a
Siamak Haschemi <siamak.h@gmx.de>
2012-03-27 15:53:51 +0200

    references #856 closes #856

---
+++ mailer-common/de.bht.fpa.mail.common/src/de/bht/fpa/mail/s000000/common/filter/IFilter.java
@@ -0,0 +1,29 @@
+package de.bht.fpa.mail.s000000.common.filter;
+
+import java.util.Set;
+
+import de.bht.fpa.mail.s000000.common.mail.model.Message;
+
+/**
+ * This interface represents {@link Message} filters. Filters can be applied to
+ * a {@link Collection} of {@link Message}s. Filters can also be combined. For
+ * example, there could be a UnionFilter, which creates the union set of two or
+ * more other Filters.
+ * 
+ * @author Siamak Haschemi
+ * 
+ */
+public interface IFilter {
+  /**
+   * This method filters the messages. It ensures that the filtered messages are
+   * in a {@link Set}, so that a {@link Message} is contained only once. The
+   * method does not modify the given list of {@link Message}s.
+   * 
+   * @param messagesToFilter
+   *          the {@link Iterable} (parent class of {@link Collection},
+   *          {@link List}, etc.) list of {@link Message}s to filter.
+   * @return the filtered {@link Set} of {@link Message}s, where each
+   *         {@link Message} is contained exactly once.
+   */
+  Set<Message> filter(Iterable<Message> messagesToFilter);
+}
