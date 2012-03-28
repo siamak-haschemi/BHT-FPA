fa0885bc1f284083d41ecd61fe0b24d17aeb4e60
Siamak Haschemi <siamak.h@gmx.de>
2012-03-29 00:39:04 +0200

    references #866 closes #866

---
+++ patterns-codebeispiele/de.bht.fpa.examples.frameworks/src/de/bht/fpa/examples/frameworks/selfcontrol/SelfControl.java
@@ -0,0 +1,32 @@
+package de.bht.fpa.examples.frameworks.selfcontrol;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import de.bht.fpa.examples.frameworks.Person;
+
+public class SelfControl {
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("What is your name? ");
+    String name = reader.readLine();
+
+    String email = "";
+    do {
+      System.out.print("What is your E-Mail address?");
+      email = reader.readLine();
+    } while (!emailIsValid(email));
+
+    Person person = new Person(name, email);
+    System.out.println(person);
+  }
+
+  private static boolean emailIsValid(String email) {
+    boolean valid = email != null && email.trim().length() > 0 && email.contains("@");
+    if (!valid) {
+      System.out.println("E-Mail is not valid!");
+    }
+    return valid;
+  }
+}
---
+++ patterns-codebeispiele/de.bht.fpa.examples.frameworks/src/de/bht/fpa/examples/frameworks/selfcontrolwithlibrary/SelfControlWithLibrary.java
@@ -0,0 +1,37 @@
+package de.bht.fpa.examples.frameworks.selfcontrolwithlibrary;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.commons.validator.routines.EmailValidator;
+
+import de.bht.fpa.examples.frameworks.Person;
+
+public class SelfControlWithLibrary {
+  public static void main(String[] args) throws Exception {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.print("What is your name? ");
+    String name = reader.readLine();
+
+    String email = "";
+    do {
+      System.out.print("What is your E-Mail address?");
+      email = reader.readLine();
+    } while (!emailIsValid(email));
+
+    Person person = new Person(name, email);
+    System.out.println(person);
+  }
+
+  private static boolean emailIsValid(String email) {
+    // use the apache commons validator library
+    EmailValidator emailValidator = EmailValidator.getInstance();
+    boolean valid = emailValidator.isValid(email);
+
+    if (!valid) {
+      System.out.println("E-Mail is not valid!");
+    }
+    return valid;
+  }
+}
