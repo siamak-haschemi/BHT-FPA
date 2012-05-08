9bd4f836c90f650c21ce0337b0e20d17c847abec
Siamak Haschemi <siamak.h@gmx.de>
2012-05-08 18:23:33 +0200

    closes #1330

--- mailer-common/de.bht.fpa.mail.common/src/de/bht/fpa/mail/s000000/common/rcp/statusbar/StatusBarHelper.java
+++ mailer-common/de.bht.fpa.mail.common/src/de/bht/fpa/mail/s000000/common/rcp/statusbar/StatusBarHelper.java
@@ -9,6 +9,7 @@
 package de.bht.fpa.mail.s000000.common.rcp.statusbar;
 
 import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.jface.action.StatusLineManager;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.IWorkbench;
@@ -25,6 +26,29 @@ import org.eclipse.ui.PlatformUI;
  * 
  */
 public final class StatusBarHelper {
+  private static class NullStatusLineManager extends StatusLineManager {
+    @Override
+    public void setMessage(String message) {
+      System.out.println("StatusLine found. Message was: " + message);
+    };
+
+    @Override
+    public void setMessage(org.eclipse.swt.graphics.Image image, String message) {
+      System.out.println("StatusLine found. Message was: " + message);
+    };
+
+    @Override
+    public void setErrorMessage(String message) {
+      System.err.println("StatusLine found. Message was: " + message);
+    };
+
+    @Override
+    public void setErrorMessage(org.eclipse.swt.graphics.Image image, String message) {
+      System.err.println("StatusLine found. Message was: " + message);
+    };
+  }
+
+  private static IStatusLineManager manager = null;
 
   /**
    * Utility classes do not need to be instantiated.
@@ -39,38 +63,49 @@ public final class StatusBarHelper {
    * 
    * @return {@link IStatusLineManager} or <code>null</code>
    */
-  public static IStatusLineManager getStatusLineManager() {
+  public static synchronized IStatusLineManager getStatusLineManager() {
+    if (manager != null) {
+      return manager;
+    }
+
+    manager = new NullStatusLineManager();
+
     IWorkbench wb = PlatformUI.getWorkbench();
     if (wb == null) {
-      return null;
+      return manager;
     }
 
     IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
     if (win == null) {
-      return null;
+      return manager;
     }
 
     IWorkbenchPage page = win.getActivePage();
     if (page == null) {
-      return null;
+      return manager;
     }
 
     IWorkbenchPart part = page.getActivePart();
     if (part == null) {
-      return null;
+      return manager;
     }
 
     IWorkbenchPartSite site = part.getSite();
     if (site == null || !(site instanceof IViewSite)) {
-      return null;
+      return manager;
     }
     IViewSite vSite = (IViewSite) site;
 
     IActionBars actionBars = vSite.getActionBars();
     if (actionBars == null) {
-      return null;
+      return manager;
     }
 
-    return actionBars.getStatusLineManager();
+    manager = actionBars.getStatusLineManager();
+    return manager;
+  }
+
+  public static void setMessage(String message) {
+    getStatusLineManager().setMessage(message);
   }
 }
