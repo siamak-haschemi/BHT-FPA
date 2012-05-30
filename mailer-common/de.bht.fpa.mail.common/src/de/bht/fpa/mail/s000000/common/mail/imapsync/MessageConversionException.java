a32e9b415e973dd5fd44d79cd0c0999cfbb96dd0
Siamak Haschemi <siamak.h@gmx.de>
2012-05-30 22:05:39 +0200

    refs #845

---
+++ mailer-common/de.bht.fpa.mail.common/src/de/bht/fpa/mail/s000000/common/mail/imapsync/MessageConversionException.java
@@ -0,0 +1,9 @@
+package de.bht.fpa.mail.s000000.common.mail.imapsync;
+
+public class MessageConversionException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public MessageConversionException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+}
---
+++ mailer-common/de.bht.fpa.mail.common/src/de/bht/fpa/mail/s000000/common/mail/imapsync/SynchronizationException.java
@@ -0,0 +1,9 @@
+package de.bht.fpa.mail.s000000.common.mail.imapsync;
+
+public class SynchronizationException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public SynchronizationException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+}
